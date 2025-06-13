1023015b43ef0156c1a91b52e2624c4535bbfb8a
Hashi-corp <harishkrishnan.p2022@vitstudent.ac.in>
2025-06-13 15:52:59 +0530

    DOM content parsed

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import streamlit as st
-from scraper import scrape_website
+from scraper import (
+    scrape_website,
+    scrape_body_content,
+    clean_body_content,
+    split_dom_content
+)
 
 st.title("Smart Web Scraper")
 url = st.text_input("Enter URL")
 
 if st.button("Scrape Site"):
     st.write("Scraping website...")
+    
     result = scrape_website(url)
-    print(result)
+    body_content = scrape_body_content(result)
+    clean_body_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = clean_body_content
+
+    with st.expander("View DOM Content"):
+        st.text_area(
+            "DOM Content",
+            clean_body_content,
+            height=300
+        )
--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 from selenium.webdriver.common.by import By
-
+from bs4 import BeautifulSoup
+import time
 
 def scrape_website(website):
     print("Launching web browser...")
@@ -10,12 +11,31 @@ def scrape_website(website):
     
     print('Connecting to Browser API...')
     sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
-    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+    with Remote(sbr_connection, options=ChromeOptions()) as driver: 
         print('Connected! Navigating...')
-        driver.get(website)
+        driver.get(website) 
         print('Navigated! Scraping page content...')
+        time.sleep(2)
+        print('HTML content scraped successfully.')
         html = driver.page_source
         return html
 
-if __name__ == '__main__':
-    scrape_website()
+def scrape_body_content(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    body_content = soup.find('body')
+    if body_content:
+        for script in body_content(['script', 'style']):
+            script.decompose()
+        return body_content.get_text(strip=True)
+    return None
+
+def clean_body_content(body_content):
+    if body_content:
+        cleaned_content = body_content.replace('\n', ' ').replace('\r', '').strip()
+        return cleaned_content
+    return None
+
+def split_dom_content(dom_content, max_length = 6000):
+    return [
+        dom_content[i: i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
