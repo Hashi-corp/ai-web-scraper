e6e2e2914a4e18013d77585dec6efb595ccc00d4
Hashi-corp <harishkrishnan.p2022@vitstudent.ac.in>
2025-06-13 16:29:50 +0530

    LLM parser set up

---
+++ ai_util.py
@@ -0,0 +1,7 @@
+import google.generativeai as genai
+
+def ai_parser(prompt, api_key):
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+    return response.text
