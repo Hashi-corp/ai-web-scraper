26072d17693359cbfa6ec469bfb1ebd00108cca2
Hashi-corp <harishkrishnan.p2022@vitstudent.ac.in>
2025-06-13 16:02:16 +0530

    LLM integ pending

--- main.py
+++ main.py
@@ -24,3 +24,15 @@ if st.button("Scrape Site"):
             clean_body_content,
             height=300
         )
+
+if "dom_content" in  st.session_state:
+    parse_description = st.text_area(
+        "describe what you want to parse"
+    )
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing content...")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            
